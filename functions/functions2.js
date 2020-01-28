2e5165f675ce1d59168ef96db543d240a72ef080
Välimäki Juhani <valju@haaga-helia.fi>
2020-01-28 11:19:08 +0200

    Improvements and fixes to functions2.js

--- functions/functions2.js
+++ functions/functions2.js
@@ -29,28 +29,30 @@ let fooar2 = name => {
 fooar2("Joe");
 
 // IIFE with arrow function
-let a,b,c,d;
 
 // 1. Anonymous function defined with arrow notation
 // 2. No params -> empty parameter list ()
 // 3. implicit (= automatic) return because of no block { } brackets
 // Returns value 42
 // That anonymous function is immediately called with the second ()
-(()=>42)();    // Definition & call
+(()=>42)();    // Definition & call, though returned 42 goes nowhere
 
 var returnedValue2 = (()=>42)();  // Definition&call, return value to a variable
+console.log("Meaning of life " +returnedValue2);
+
+console.log((()=>42)());  // Tricky test for code reading
 
 (()=>{return 42;})();  // The same IIFE A bit longer way, with explicit return
 
-//console.log(   (name => "Hello " + name("Joe"))   );
-console.log(   (name => "Hello " + name)("Joe")   );
+console.log(   ((name) => "Hello " + name)("Joe")   );   // version 1
+console.log(   (name => "Hello " + name)("Joe")   );     // version 2
 
 
 // REMEMBER THESE FOUR FACTS about arrow functions:
 
 // 1. parameter list options:
 //   0 parameters:        ()
-//   1 parameter, Note!:  (name) or name  
+//   1 parameter, Note!:  (name)   OR   name  
 //   2 or more parameters (x,y)
 
 // 2. if no function body {  } then 
@@ -60,17 +62,22 @@ console.log(   (name => "Hello " + name)("Joe")   );
 // (   ) are needed around the object, otherwise 
 // {} would be confused for a block:
       () => ({a:123, b:456})
+      // () => {a:123, b:456}   // This would be the problem!
+
 // 4. if using the keyword 'this', the 'this' refers to the outer object
 // and not to the function object itself! 
 // -> sometimes you need arrow function, and normal would not work!!!
-// Typically in some kind of event-handler function
+// (In traditional function code, 'this' refers to the function object
+// itself. Big difference)
+// Typically in some kind of event-handler function we MUST
+// use arrow function and traditional with same code would not work.
 
 console.log("Put a break point here for stopping the debugger");
 
 
-/*
+/*   Minor thing:
 
-// This IIFE with arrow function takes 11-13 characters to write
+// This code with arrow function IIFE takes 11-13 characters to write
 let answer = (()=>42)();
 
 // Writing THE SAME with traditional syntaxt takes 24-26 more
