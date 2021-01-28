2266608a18b5bfd734360b708a55a4c85ac4e679
valju <juhani.valimaki@haaga-helia.fi>
2021-01-28 11:47:49 +0200

    Some more edits

--- functions/functions3.js
+++ functions/functions3.js
@@ -6,6 +6,7 @@ console.log("********** functions 3 - default values **********");
 console.log("**************************************************");
 
 // Default parameter values for functions    -    Used if not enough arguments provided in call
+// => leads to undefined parameter value   (undefined can be also specifically defined)
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters
 
 var sum = function (num1, num2=3) {
@@ -30,3 +31,20 @@ console.log(sum(4,null));
 // Hint: What was the case when default value used?
 
 console.log("Put a break point here for stopping the debugger");
+
+
+/*  // Extra extra, forget: Find a browser or other JS environment where this mispelled
+    // lenght (instead of length) would still lead to loop run once, for 0.
+    // Seems to fixed at least in Chrome
+
+    // Problem was that 0 was somehow considered "less than" undefined
+    // array.lenght gives undefined, as there is no such member, there is .lenght
+    // It's likely the language did not even have definition on how to
+    // compare number and undefined with the < operator.
+
+var array = [1,2,3];
+for(var i=0; i<array.lenght; i++ ) {
+    console.log(i +" : " + array[i]);
+}
+*/
+
