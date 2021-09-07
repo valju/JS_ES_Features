e544b7e2a21105d4b45a1746bb98c104ea0f9c86
Välimäki Juhani <valju@haaga-helia.fi>
2021-09-07 13:10:58 +0300

    Object visualization images added

--- objects/objects1b_shallow_copy.js
+++ objects/objects1b_shallow_copy.js
@@ -13,6 +13,10 @@ let bee = {name:"Joe", yob:1966};
 let cee = [aa, bee];
 let copy = [...cee];
 
+// You tried to make a separate copy and keep all
+// original information safe. Any problems?
+// You'll need to draw the objects and references to see.
+
 // Deep or shallow copy?
 // Deep copy = everything duplicated and thus separated from original
 // Shallow copy = just first level reference variables are separate,
