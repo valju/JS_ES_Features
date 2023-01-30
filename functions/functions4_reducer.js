4e9f449c5b77bd8a54f917292883eacd76df6673
valju <juhani.valimaki@haaga-helia.fi>
2023-01-30 14:31:17 +0200

    More explanation added to the reducer example

--- functions/functions4_reducer.js
+++ functions/functions4_reducer.js
@@ -9,7 +9,17 @@ const myReducer = (previousValue, currentValue) => {
 console.log(array1.reduce(myReducer));
 // expected output: 10
 
+// 1,2 => 3
+// 3,3 => 6
+// 6,4 => 10
+// our reducer function run 3 times! (because no initial value given, first used as initial)
 
 // 5 + 1 + 2 + 3 + 4
 console.log(array1.reduce(myReducer, 5));
 // expected output: 15
+
+// 5,1 => 6
+// 6,2 => 8
+// 8,3 => 11
+// 11,4 => 15  
+// our reducer function run 4 times! (because initial value given, first used as first nextValue)
